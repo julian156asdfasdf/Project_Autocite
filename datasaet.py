e7d91a2d3e1ce503f9300c9a565c2e8862f6aea1
Julian Oll <s203929@student.dtu.dk>
2024-02-28 15:31:50 +0100

    datasaet.py added med funktioner

--- datasaet.py
+++ datasaet.py
@@ -15,6 +15,8 @@ import requests
 import tarfile
 import os
 
+
+
 def read_arxiv_papers(path):
     return pd.read_csv(path)
 
@@ -32,49 +34,65 @@ def download_paper(url, path):
 V1_URL = 'https://github.com/paperswithcode/axcell/releases/download/v1.0/'
 ARXIV_PAPERS_URL = V1_URL + 'arxiv-papers.csv.xz'
 arxiv_papers = read_arxiv_papers(ARXIV_PAPERS_URL)
+arxiv_papers = arxiv_papers[arxiv_papers['status']=='success']
 
 links = arxiv_papers.apply(get_eprint_link, axis=1)
 
-# Specify the directory where you want to save the papers
-save_dir = Path("./papers")
-save_dir.mkdir(parents=True, exist_ok=True)
 
-for idx, link in enumerate(links[:40], start=1):  # Just an example with `.tail()`, remove it to download all
-    paper_id = arxiv_papers.iloc[idx - 1].arxiv_id  # Adjust index if necessary
-    file_path = save_dir / f"{paper_id}.tar"
-    print(f"Downloading {paper_id} to {file_path}...")
-    download_paper(link, file_path)
-
-
-export_dir = Path("./tex_files")
-export_dir.mkdir(parents=True, exist_ok=True)
 #%%
-
-import tarfile
-import os
-# Path to the directory containing your tar files
-directory_path = r'/Users/julianoll/Desktop/Fagprojekt/Project_Autocite'
+# Specify the directory where you want to save the papers
 
 
-# List all files in the directory
-files = os.listdir(export_dir)
-for file_name in files:
-    if file_name[-4:]=='.tar':
-        try:
-            # Create a directory for each tar file
-            tar_dir_name = os.path.splitext(file_name)[0]  # Use the file name without extension as the directory name
-            tar_dir_path = os.path.join(export_dir, tar_dir_name)
-            os.makedirs(tar_dir_path, exist_ok=True)  # Create directory if it doesn't exist
-            
-            # Open the tar file and extract its contents into the newly created directory
-            with tarfile.open(os.path.join(directory_path, file_name), 'r:') as tar:
-                tar.extractall(path=tar_dir_path)
-            #os.remove(os.path.join(directory_path, file_name))
+def get_docs(dir ,links, k=40):
+    for idx, link in enumerate(links[:k], start=1):  # Just an example with `.tail()`, remove it to download all
+        paper_id = arxiv_papers.iloc[idx - 1].arxiv_id  # Adjust index if necessary
+        file_path = dir / f"{paper_id}.tar"
+        print(f"Downloading {paper_id} to {file_path}...")
+        download_paper(link, file_path)
 
-            print(f"{file_name} extracted successfully to {tar_dir_path}.")
-        except tarfile.ReadError as e:
-            print(f"Error extracting {file_name}: {e}")
+
+def tex_docs(save_dir,export_dir):
+    files = os.listdir(save_dir)
+    for file_name in files:
+        if file_name[-4:]=='.tar':
+            try:
+                #creates a folder in the directory folder for each paper
+                tar_dir_name = os.path.splitext(file_name)[0] 
+                tar_dir_path = os.path.join(export_dir, tar_dir_name)
+                os.makedirs(tar_dir_path, exist_ok=True) 
+                
+                # Open the tar file and extract its contents into the newly created directory
+                with tarfile.open(os.path.join(save_dir, file_name), 'r') as tar:
+                    tar.extractall(path=tar_dir_path)
+                #os.remove(os.path.join(save_dir, file_name))
+
+
+
+                print(f"{file_name} extracted successfully to {tar_dir_path}.")
+            except tarfile.ReadError as e:
+                print(f"Error extracting {file_name}: {e}")
+    
+
+def rmv_non_tex_files(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if not file.lower().endswith(".tex"):
+                file_path = os.path.join(root, file)
+                os.remove(file_path)
+                print(f"Removed non-tex file: {file_path}")
+
+
 
 
 
 # %%
+save_dir = Path("./papers")
+export_dir = Path("./tex_files")
+#directory_path = r'/Users/julianoll/Desktop/Fagprojekt/Project_Autocite/papers'
+
+get_docs(save_dir, links)
+
+tex_docs(save_dir, export_dir)
+
+rmv_non_tex_files(export_dir)
+
