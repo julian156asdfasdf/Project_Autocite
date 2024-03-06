7ea38509887c7ca9d5acc0479d8c15d303aef377
Willz789 <williammistarz@gmail.com>
2024-03-06 15:24:30 +0100

    Added description to function Bib_to_id

--- Bib_to_id.py
+++ Bib_to_id.py
@@ -1,6 +1,10 @@
 import urllib, urllib.request
 
-def bib_to_id(title_str, start_idx_str, max_idx_str, authors_str):
+def bib_to_id(title_str, start_idx_str, max_idx_str, authors_str = ''):
+    """
+    This func takes a title as a string, a list of the authors, which can be empty, and the start and max indexes for the search. 
+    It returns the arXiv id of the paper if it exists and there exists only one match for the API search, and None otherwise.
+    """
     url = 'http://export.arxiv.org/api/query?search_query=au:' + authors_str + '+AND+ti:%22' + title_str + '%22&start=' + start_idx_str + '&max_results=' + max_idx_str
     data = urllib.request.urlopen(url)
     data_str = data.read().decode('utf-8')
