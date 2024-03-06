fbe6a2cb6a6cdf0377b2c6447391eca7b4fa60d5
Julian Oll <s203929@student.dtu.dk>
2024-03-06 10:07:57 +0100

    Laver en tabel af slettede filer fra preprocessing og skriver det som en txt fil ligesom i Tues wiki side

--- datasaet.py
+++ datasaet.py
@@ -42,7 +42,7 @@ links = arxiv_papers.apply(get_eprint_link, axis=1)
 # Specify the directory where you want to save the papers
 
 
-def get_docs(dir ,links, k=40):
+def get_docs(dir ,links, k=5):
     for idx, link in enumerate(links[:k], start=1):  # Just an example with `.tail()`, remove it to download all
         paper_id = arxiv_papers.iloc[idx - 1].arxiv_id  # Adjust index if necessary
         file_path = dir / f"{paper_id}.tar"
@@ -77,6 +77,9 @@ def delete_empty_folders(root):
     deleted = set()
     
     for current_dir, subdirs, files in os.walk(root, topdown=False):
+        print(current_dir)
+        print(subdirs)
+        print(files)
 
         still_has_subdirs = False
         for subdir in subdirs:
@@ -91,13 +94,25 @@ def delete_empty_folders(root):
     return deleted
 
 def rmv_non_tex_files(directory):
+    del_dict = {}
+    del_files_path = Path("./del_files")
+    for dir in os.listdir(directory):
+        os.makedirs(os.path.join(del_files_path, dir),exist_ok=True)
+        del_dict[dir] = set()
+
+
     for root, dirs, files in os.walk(directory):
+        print('root', root)
+        print('dirs', dirs)
+        print('files', files)
         for file in files:
+
             if not file.lower().endswith(".tex") and not file.lower().endswith(".bib") and not file.lower().endswith(".bbl"):
                 file_path = os.path.join(root, file)
                 os.remove(file_path)
                 print(f"Removed non-tex file: {file_path}")
     delete_empty_folders(directory)
+    return del_dict[dir]
 
 
 
@@ -113,6 +128,10 @@ get_docs(save_dir, links)
 
 tex_docs(save_dir, export_dir)
 
+#%%
+dir = rmv_non_tex_files(export_dir)
+
+#%%
 from data_count import count_ref
 
 bbl_count, bib_count = count_ref(export_dir)
@@ -123,6 +142,10 @@ print(f"Number of .bib files: {bib_count}")
 
 
 
-# %%
-
+data = {'ID': ['bob', 'lis'], 'Deleted_documents': [14, 16], 'girth': [15,16]}
+import tabulate
+s = tabulate.tabulate(data, headers='keys')
+print(s)
+with open("output.txt", 'w') as f:
+   f.write(s)
 # %%
