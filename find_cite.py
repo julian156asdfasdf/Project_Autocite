43fea6daee5b9d14bc1adec46332bbc207001447
Nikolai <nbenemark@gmail.com>
2024-03-12 18:36:44 +0100

    Added functionality to work with \footcite, \citep and \citet (and actually updated the function name this time)

--- find_cite.py
+++ find_cite.py
@@ -1,6 +1,6 @@
-def find_cite(tex_file):
+def isolate_cites(tex_file):
     """
-    Find every instance of \cite{} in a .tex file and inserts \n before and after the instance.
+    Find every instance of citations (\cite{}, \footcite{}, \citep, etc.) in a .tex file and inserts \n before and after the instance.
 
     Arguments:
     tex_file: The .tex file to be searched for citations.
@@ -13,11 +13,20 @@ def find_cite(tex_file):
 
     with open(tex_file, 'w') as f:
 
-        # For every line in the file, if the line contains \cite{}, add \n before and after the citation
+        # For every line in the file, if the line contains a citation, add \n before and after the citation
         for line in lines:
-            if "\\cite{" in line:
-                line = line.replace("\\cite{", "\n \\cite{")
-                line = line.replace("}", "}\n ")
+            if "\\cite{" in line: # \cite
+                line = line.replace("\\cite{", "\n\\cite{")
+                line = line.replace("}", "}\n")
+            if "\\footcite{" in line: # \footcite
+                line = line.replace("\\footcite{", "\n\\footcite{")
+                line = line.replace("}", "}\n")
+            if "\\citep{" in line: # \citep
+                line = line.replace("\\citep{", "\n\\citep{")
+                line = line.replace("}", "}\n")
+            if "\\citet{" in line: # \citet
+                line = line.replace("\\citet{", "\n\\citet{")
+                line = line.replace("}", "}\n")
             f.write(line)
             
     return None
