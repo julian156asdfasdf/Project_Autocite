2a8f9bea058388f992f551a15d83da2f56dcac47
Nikolai <nbenemark@gmail.com>
2024-04-08 16:56:06 +0200

    Fixed bug where multiple newlines would be added, if multiple types of citations appeared in the same line

--- find_cite.py
+++ find_cite.py
@@ -1,3 +1,5 @@
+import re
+
 def isolate_cites(tex_file):
     """
     Find every instance of citations (\cite{}, \footcite{}, \citep, etc.) in a .tex file and inserts \n before and after the instance.
@@ -16,17 +18,13 @@ def isolate_cites(tex_file):
         # For every line in the file, if the line contains a citation, add \n before and after the citation
         for line in lines:
             if "\\cite{" in line: # \cite
-                line = line.replace("\\cite{", "\n\\cite{")
-                line = line.replace("}", "}\n")
+                line = re.sub(r"(\\cite{.*?})", r"\n\1\n", line)
             if "\\footcite{" in line: # \footcite
-                line = line.replace("\\footcite{", "\n\\footcite{")
-                line = line.replace("}", "}\n")
+                line = re.sub(r"(\\footcite{.*?})", r"\n\1\n", line)
             if "\\citep{" in line: # \citep
-                line = line.replace("\\citep{", "\n\\citep{")
-                line = line.replace("}", "}\n")
+                line = re.sub(r"(\\citep{.*?})", r"\n\1\n", line)
             if "\\citet{" in line: # \citet
-                line = line.replace("\\citet{", "\n\\citet{")
-                line = line.replace("}", "}\n")
+                line = re.sub(r"(\\citet{.*?})", r"\n\1\n", line)
             f.write(line)
             
     return None
