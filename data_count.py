2d199e60f71fa5e1a052d0754ffb1ca3db624e90
Nicklas <nicklasmundt@gmail.com>
2024-04-17 14:37:55 +0200

    small changes

--- data_count.py
+++ data_count.py
@@ -11,6 +11,7 @@ def count_ref(directory):
     """
     bbl_count = 0
     bib_count = 0
+    tex_count = 0
 
     for _, _, files in os.walk(directory):
         for file in files:
@@ -18,14 +19,18 @@ def count_ref(directory):
                 bbl_count += 1
             elif file.endswith(".bib"):
                 bib_count += 1
+            elif file.endswith(".tex"):
+                tex_count += 1
 
-    return bbl_count, bib_count
+    return bbl_count, bib_count, tex_count
 
 if __name__ == "__main__":
     current_dir = os.path.dirname(os.path.abspath(__file__))
     directory = current_dir
-    bbl_count, bib_count = count_ref(directory)
+    directory = 'Step_1'
+    bbl_count, bib_count, tex_count = count_ref(directory)
     dir_count = len(os.listdir(directory))
     print(f"Number of .bbl files: {bbl_count}")
     print(f"Number of .bib files: {bib_count}")
+    print(f"Number of .tex files: {tex_count}")
     print(f"Number of directories: {dir_count}")
