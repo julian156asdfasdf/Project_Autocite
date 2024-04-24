0c2d65c919648366dca758da4b14a5baa0106085
Willz789 <williammistarz@gmail.com>
2024-04-24 22:03:03 +0200

    Commented the code in parseBBL. Finished the code for generating randomized subset of KaggleDB.

--- RandomizeKaggleDB.py
+++ RandomizeKaggleDB.py
@@ -1,67 +1,88 @@
-import pandas as pd
 import random
 import pickle
 import json
+import re
+from tqdm.auto import tqdm
 
 from parseBBL2 import remove_latex_commands
 
 
-def randomizeKaggleDBjson():
+def randomizeKaggleDB(filepath = "Kaggle_Dataset.json"):
     # Load the Kaggle dataset
-    with open("Kaggle_Dataset.json", 'r') as file:
+    with open(filepath, 'r') as file:
         KaggleDB = file.readlines()
         random.seed(42)
         random.shuffle(KaggleDB)
     
     LatexCleanedKaggleDB = []
-    for i in range(len(KaggleDB)):
+    rows_total = len(KaggleDB)
+    for i in tqdm(range(rows_total)):
         row = KaggleDB[i]
-        if i == len(KaggleDB) - 1:
+        # Load the json string into a dictionary
+        if i == rows_total - 1:
             dictionary = json.loads(row)
         else:
             dictionary = json.loads(row[:-1])
-        arxiv_id = dictionary['id']
-        title = remove_latex_commands(dictionary['title'])
-        authors = remove_latex_commands(dictionary['authors'])
-        publish_date = dictionary['publish_time']
-        info = authors + " " + title + " " + publish_date
-        LatexCleanedKaggleDB.append({"arxiv_id": arxiv_id, "info": info, "title": title, "authors": authors, "publish_date": publish_date})
-    try:
-        with open("Randomized_Kaggle_Dataset.json", 'w') as file:
-            json.dump(KaggleDB, file)
-    except Exception as e:
-        print(f"Failed to save the randomized dataset with error: {e}")
+        
+        # Extracts only the rows with the specified category
+        if "physics" in dictionary['categories']: # others are 'math':650k, 'cs':850k, 'stat', 'eess', 'physics':250k, 'q-bio', 'q-fin', 'quant-ph', 'hep':350k
+            # Extract the relevant information
+            arxiv_id = dictionary['id']
+            title = remove_latex_commands(dictionary['title']) # Remove all latex commands from the title
+            authors = remove_latex_commands(dictionary['authors']) # Remove all latex commands from the authors
+            authors = re.sub(r'\s+', ' ', authors)
+            update_date = dictionary['update_date']
+            info = authors + ". " + re.sub(r'\s+', ' ', title) + ". " + update_date
+            # Remove all equations from title
+            idx_dollar = 0
+            while "$" in title[idx_dollar:]:
+                idx_dollar = title.find("$", idx_dollar)
+                if idx_dollar == len(title)-1:
+                    break
+                if title[idx_dollar-1] == "\\":
+                    idx_dollar = idx_dollar+1
+                elif title[idx_dollar+1] == "$":
+                    end_dollar_idx = title.find("$", idx_dollar+2)+1
+                    if end_dollar_idx == 0:
+                        end_dollar_idx = len(title)
+                    title = title[:idx_dollar] + title[end_dollar_idx+1:]
+                else:
+                    end_dollar_idx = title.find("$", idx_dollar+1)
+                    if end_dollar_idx == -1:
+                        end_dollar_idx = len(title)
+                    title = title[:idx_dollar] + title[end_dollar_idx+1:]
+            
+            title = re.sub(r'\s+', ' ', title).strip() # Remove all extra whitespaces
+            # Append the information to the new dataset as a dictionary
+            LatexCleanedKaggleDB.append({"arxiv_id": arxiv_id, "info": info, "title": title, "authors": authors})
+
+     # Writing the randomized dataset to a new json file       
+    rows = len(LatexCleanedKaggleDB)
+    with open("Randomized_Kaggle_Dataset_Subset_Physics.json", 'w') as file:
+        for i in tqdm(range(rows)):
+            json.dump(LatexCleanedKaggleDB[i], file)
+            if i != rows-1:
+                file.write('\n')  # Add a newline after each dictionary
 
 
-def randomizeKaggleDBpickle():
-    # Load the Kaggle dataset
-    with open("Kaggle_Dataset.json", 'r') as file:
-        KaggleDB = file.readlines()
-        random.seed(42)
-        random.shuffle(KaggleDB)
-    
-    # Randomize the dataset
-    
-    # Save the randomized dataset
+def read_KaggleDB(filepath = "Randomized_Kaggle_Dataset_Subset_Physics.json"):
+    # Load the json file into a list of dictionaries
+    KaggleDB = []
     try:
-        with open("Randomized_Kaggle_Dataset.pkl", 'wb') as file:
-            pickle.dump(KaggleDB, file)
-    except Exception as e:
-        print(f"Failed to save the randomized dataset with error: {e}")
-
-def read_randomizedKaggleDBpickle():
-    # Load the randomized dataset
-    try:
-        with open("Randomized_Kaggle_Dataset.pkl", 'rb') as file:
-            KaggleDB = pickle.load(file)
+        with open(filepath, 'r') as file:
+            KaggleDB_raw = file.readlines()
+            rows = len(KaggleDB_raw)
+            for i in tqdm(range(rows)):
+                if i == rows - 1:
+                    dictionary = json.loads(KaggleDB_raw[i])
+                else:
+                    dictionary = json.loads(KaggleDB_raw[i][:-1])
+                KaggleDB.append(dictionary)
     except Exception as e:
         print(f"Failed to load the randomized dataset with error: {e}")
         KaggleDB = []
-    print(KaggleDB[:50])
-    print("")
-    print(len(KaggleDB))
-    pass
+    return KaggleDB
 
-#randomizeKaggleDB()
-#read_randomizedKaggleDB()
-randomizeKaggleDBjson()
+if __name__ == '__main__':
+    randomizeKaggleDB()
+    KaggleDB = read_KaggleDB()
--- parseBBL2.py
+++ parseBBL2.py
@@ -1,41 +1,54 @@
 import time
-#from thefuzz import fuzz
 import re
 
-#from pylatexenc.latex2text import LatexNodes2Text
-
-
-#ACCENT_CONVERTER = LatexNodes2Text()
-
 def remove_latex_commands(text):
+    """
+    Removes all latex commands from a string.
+    """
     text = text.replace("et~al.", " ").replace("\n", ' ')
+
+    # Assigns the intervals of the equations in the text (Commands are not to be removed from them)
     equation_indexes = []
     last_dollar_idx = -1
     while "$" in text[last_dollar_idx+1:]:
         idx_dollar = text.find("$", last_dollar_idx+1)
+        # If the dollar sign is the last character in the text, then break
+        if idx_dollar == len(text)-1:
+            break
+        # If the dollar sign is a command (Meaning not an equation)
         if text[idx_dollar-1] == "\\":
             last_dollar_idx = idx_dollar
+        # If there are two dollar signs in a row, then it is an row-equation
         elif text[idx_dollar+1] == "$":
             last_dollar_idx = text.find("$$", idx_dollar+2)+1
+            last_dollar_idx = (last_dollar_idx if last_dollar_idx != 0 else len(text))
             equation_indexes.append([idx_dollar, last_dollar_idx])
-        else:
+        else: # If there is only one dollar sign, then it is an inline-equation
             last_dollar_idx = text.find("$", idx_dollar+1)
+            last_dollar_idx = (last_dollar_idx if last_dollar_idx != -1 else len(text))
             equation_indexes.append([idx_dollar, last_dollar_idx])
     
-
+    # Checks if the index is within an equation after some text might been removed
     def check_if_in_equation(idx, equation_indexes, text_subtraction_size):
         for i in range(len(equation_indexes)):
             if idx >= equation_indexes[i][0]-text_subtraction_size and idx <= equation_indexes[i][1]-text_subtraction_size:
                 return True
         return False
     
+    # Remove all latex commands
     text_subtraction_size = 0
     idx_backslash = text.find("\\")
     while idx_backslash != -1:
+        # If the backslash is the last character in the text, then break
+        if idx_backslash == len(text)-1:
+            text = text[:idx_backslash]
+            break
+        # If the backslash is part of an equation, then skip
         if check_if_in_equation(idx_backslash, equation_indexes, text_subtraction_size):
             idx_backslash = text.find("\\", idx_backslash+1)
             continue
-        break_chars = [" ", "{", "}", "\"", "\'", "´", "`", "\\", "_", "^"]
+
+        # If the backslash is simply to write a special character, then remove it the backslash and keep the special character
         if text[idx_backslash+1] in ["\'", "`", "´"]:
             text = text[:idx_backslash] + text[idx_backslash+1:]
             for i in range(len(equation_indexes)):
@@ -43,23 +56,27 @@ def remove_latex_commands(text):
                 equation_indexes[i][1] -= 1
             idx_backslash = text.find("\\", idx_backslash+1)
             continue
+
+        # Define the characters that can break the command
+        break_chars = [" ", "{", "}", "\"", "\'", "´", "`", "\\", "_", "^"]
         end_backslash_idx = len(text)
         extra_chars_removed = 0
         for break_char in break_chars:
-            if text.find(break_char, idx_backslash+1) != -1:
+            if text.find(break_char, idx_backslash+1) != -1: # If the break_char is in the text after the backslash
+                # If the new break_char appears closer to the backslash than the previous one, then update the end_backslash_idx
                 if text.find(break_char, idx_backslash+1) < end_backslash_idx and text.find(break_char, idx_backslash+1) > idx_backslash:
                     end_backslash_idx = text.find(break_char, idx_backslash+1)
-                    if break_char in ["`", "´", "\""]:
+                    if break_char in [" ", "`", "´", "\""]:
                         extra_chars_removed = 1
-                    elif break_char in ["\\", "}", "_", "^", "`", "´"]: 
+                    elif break_char in ["\\", "}", "_", "^", "`", "´"]:  # In these cases keep the name of the command
                         extra_chars_removed = -(end_backslash_idx-idx_backslash)+1
                     else:
                         extra_chars_removed = 0
         
-        text = text[:idx_backslash] + text[end_backslash_idx+extra_chars_removed:]
+        text = text[:idx_backslash] + text[end_backslash_idx+extra_chars_removed:] # Remove the command from the text
         text_subtraction_size += end_backslash_idx-idx_backslash+extra_chars_removed
-        idx_backslash = text.find("\\", idx_backslash)
-    return text.replace("{","").replace("}","").replace("\\ ", " ").strip()
+        idx_backslash = text.find("\\", idx_backslash) # Find the next backslash
+    return text.replace("{","").replace("}","").replace("\\ ", " ").strip() # Clean the string and return it
 
 
 def get_first_author_lastname_from_info(info):
@@ -86,17 +103,21 @@ def get_first_author_lastname_from_info(info):
         info_search = info_search[:first_dot].strip()
     first_author_name = info_search
 
-    # Removes all latex comments in the text
-    first_author_lastname = remove_latex_commands(first_author_name)
     # Retrieve the last of the names
-    first_author_lastname = first_author_lastname.split(" ")[-1].split("~")[-1].strip()
+    first_author_lastname = first_author_name.split(" ")[-1].split("~")[-1].strip()
     return first_author_lastname if len(first_author_lastname) > 1 else None
 
 
 def parsebbl(file_path = None, bbl_str = None):
     """
-    Parses a bbl file and returns a dictionary with the latex \cite-key as the keys and the value is a dict with title and authors as strings.
+    Parses a bbl file and returns a dictionary with the latex \cite-key as the keys and the value is a dict containing:
+        string : arxiv_id if possible else its None
+        string : title = None (For easier comparison with the parseBib function)
+        string : first_authors_lastname (For possible easier search through KaggleDB)
+        string : info = the rest of the information in the bbl file
     """
+
+    # Read the bbl file or bbl string
     if file_path:
         try:
             with open(file_path, 'r', encoding='ISO-8859-1') as file:
@@ -108,6 +129,7 @@ def parsebbl(file_path = None, bbl_str = None):
         print("No input given to the parsebbl function. Exiting.")
         return {}
     
+    # Check if the file contains a bibliography
     if "\\begin{thebibliography}" not in bbl_str:
         print("No bibliography found in the bbl file. Exiting.")
         return {}
@@ -120,37 +142,52 @@ def parsebbl(file_path = None, bbl_str = None):
     # Remove lines starting with "\\providecommand"
     cleaned_str = re.sub(r"\\providecommand.*\n", "", cleaned_str)
 
+    # Splits the bbl file into the different bibitems
     ss = cleaned_str.split(r"\bibitem")[1:]
     
-    bbl_dict = {}
+    bbl_dict = {} # The dictionary to be returned
+
+    # Processes each bibitem
     for s in ss:
         arxiv_id = None
         first_author_lastname = None
-        # Case 1
+        # Case 1 (If the bibitem starts with a bracket)
         if s.strip().startswith("["):
             j = s.find("]")
 
-            ref_name = s[j:].split("}")[0].replace("]", '').replace("{", '')
-            info = s[j:]
+            # The ref name is the string inside the curly brackets following the square brackets
+            info = s[j+1:]
+            ref_name = info.split("}")[0].replace("{", '')
             info = info[info.find("}")+1:].strip()
 
             # Subcase 1
+            # If the info contains \newblock or \newline, then parse as such
             if r"\newblock" in cleaned_str or r"\newline" in cleaned_str:
-                info = remove_latex_commands(info)
-                # info = info.replace(r"\newblock", ' ').replace(r"\newline", ' ').replace(r"\em", ' ').replace(r"\emph", '').replace(r"\textbf", '')
+                # Remove urls
+                idx_url = info.find("\\url")
+                if idx_url != -1:
+                    info = info[:idx_url] + info[info.find("}", idx_url)+1:]
+                info = remove_latex_commands(info) 
                 first_author_lastname = get_first_author_lastname_from_info(info)
 
             # Subcase 2
+            # If the info starts with a curly bracket, then parse as such
             elif info.startswith("{"):
+                # If the info contains \eprint, then extract the arxiv_id
                 if info.find("\\eprint{") != -1:
                     arxiv_id = info.split("\\eprint{")[-1].split("}")[0]
-                first_author_lastname = info[1:info.find("}")].split(" ")[-1]
+                    continue
+                first_author_lastname = info[1:info.find("}")].split(" ")[-1] # Get the last name of the first author (It is within the first curly brackets)
                 info = remove_latex_commands(info)
+
             # Subcase 3
+            # If the info contains \bibfield, then parse as such
             elif "\\bibfield" in info:
-                info = info.split("\\BibitemOpen")[-1].split("\\BibitemShut")[0].strip()
+                info = info.split("\\BibitemOpen")[-1].split("\\BibitemShut")[0].strip() # Remove the \BibitemOpen and \BibitemShut commands
                 try:
+                    # Get the beginning of the first author's last name 
                     first_author_lastname = info[info.find("{",info.find("\\bibnamefont"))+1:]
+                    # Find the end of the first author's last name
                     start_brackets = 0
                     for i in range(len(first_author_lastname)):
                         char = first_author_lastname[i]
@@ -164,33 +201,44 @@ def parsebbl(file_path = None, bbl_str = None):
                     first_author_lastname = remove_latex_commands(first_author_lastname)
                 except:
                     first_author_lastname = None
+                # Remove all \bibinfo and \bibfield commands
                 while "\\bibinfo" in info:
                     idx = info.find("\\bibinfo")
                     info = info[:idx] + info[info.find("}", idx)+1:]
                 while "\\bibfield" in info:
                     idx = info.find("\\bibfield")
                     info = info[:idx] + info[info.find("}", idx)+1:]
+                # Remove all \url commands
                 idx_href = info.find("\\href")
                 if idx_href != -1:
                     info = info[:idx_href] + info[info.find("}", idx_href)+1:]
                 info = remove_latex_commands(info)
-                # In some occatinos \bibnamefont isn't removed
-
 
         # Case 2
+        # If the bibitem starts with a curly bracket
         elif s.strip().startswith("{"):
+            # Remove unnecessary characters
             s = s.replace(r"\newline", " ").replace(r"\newblock", ' ').replace("\n", ' ')
+            # The ref name is the string inside the first set of curly brackets
             ref_name = s[s.find("{")+1:s.find("}")]
             info = s[s.find("}")+1:]
+            # remove urls
+            idx_url = info.find("\\url")
+            if idx_url != -1:
+                info = info[:idx_url] + info[info.find("}", idx_url)+1:]
+            info = remove_latex_commands(info)
             first_author_lastname = get_first_author_lastname_from_info(info)
 
-        info = re.sub(r'\s+', ' ', info)
+
+        info = re.sub(r'\s+', ' ', info) # Remove all extra whitespaces
+        # Append the information to the dictionary
         bbl_dict[ref_name] = {"title":None, "info": info, "first_author_lastname": first_author_lastname, "ArXiV-ID": arxiv_id}
 
     return bbl_dict
 
 
 if __name__ == "__main__":
+    # All the test files
     filepaths = [
         "Step_1/0809.0840/heralhc_jhepwork.bbl", 
         "Step_1/1601.03559/paper_info.bbl", 
