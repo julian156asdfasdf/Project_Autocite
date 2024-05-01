eeb6a19d6bd601b9074ba39edc4e2d002c8e3836
Julian <s203929@dtu.dk>
2024-05-01 10:48:08 +0200

    better timecomplexity factor 100

--- step3_processing.py
+++ step3_processing.py
@@ -15,19 +15,18 @@ class proccessing_3:
     def __init__(self, database_url, file_dir):
         self.kaggle_db = pd.read_json(database_url, lines=True)
         self.file_dir = file_dir
-
+        self.author_db = defaultdict(list)
     def subdivide_by_author(self):
         # method 1, each key is name of author
-        author_db = defaultdict(list)
         i = 0
-        for authors in self.kaggle_db['authors']:
+        for index, row in self.kaggle_db.iterrows():
+            authors = row['authors']
+            arxiv_id = row['arxiv_id']
             for author in authors.split(','):
-                author_db[author].append(self.kaggle_db[self.kaggle_db['authors']==authors]['arxiv_id'])
-
-            i+=1
-            if i == 1000:
-                break
-        return author_db
+                self.author_db[author].append(arxiv_id)
+                i+=1
+            
+        return self.author_db
 
         #
         #for dir in os.listdir(self.file_dir):
