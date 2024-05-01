04377dddc2b00646a0c1ee0a6adea6b143068cd5
Julian <s203929@dtu.dk>
2024-05-01 11:23:42 +0200

    function that checks how many of authors in refereces.json can be found in author_db does not work yet

--- step3_processing.py
+++ step3_processing.py
@@ -17,13 +17,28 @@ class proccessing_3:
         self.file_dir = file_dir
         self.author_db = defaultdict(set)
     def subdivide_by_author(self):
-        # method 1, each key is name of author
+        """ 
+        Argument: Kaggle_database
+        Output: Author_db a dictionary
+        This function takes the kaggle database, and makes a dictionary
+        where each key is the name of an author
+        and the value is a list of every article they have made
+        where the value in the list is the arxiv id 
+        """
+
         for index, row in self.kaggle_db.iterrows():
             authors = row['authors']
             arxiv_id = row['arxiv_id']
             for author in authors.split(','):
                 self.author_db[author].add(arxiv_id)
         
+        # Problem is that the authors keys are not good example
+        # the following are the different keys:  " 'Eric H'ebrard"," 'Eric Herbert (LIED)"," 'Eric Herbert and Pierre-Philippe Cortet",
+        # Can be seen when doing sorted(self.author_db)
+        # possible solution, make it a set instead of list, and if two keys contain the same, then union between long and short key on the short one
+        # do not collapse the long one as it can run multiple times
+        # possible implementation above, problem is that it runs incredibly slow
+
         #i = 50
         #succesfull_iterations = []
         #for author in self.author_db.keys():
@@ -34,20 +49,22 @@ class proccessing_3:
         #            i+=10
         return self.author_db
     
-        # Problem is that the authors keys are not good example
-        # the following are the different keys:  " 'Eric H'ebrard"," 'Eric Herbert (LIED)"," 'Eric Herbert and Pierre-Philippe Cortet",
-        # Can be seen when doing sorted(self.author_db)
-        # possible solution, make it a set instead of list, and if two keys contain the same, then union between long and short key on the short one
-        # do not collapse the long one as it can run multiple times
-        # possible implementation above, problem is that it runs incredibly slow
+       
 
-        #
-        #for dir in os.listdir(self.file_dir):
-        #    author_db = defaultdict()
-        #    path = Path(os.path.join(self.file_dir,dir, 'references.json'))
-        #    ref_json = pd.read_json(path, lines=True)
-        #    for author in ref_json['author_ln']:
-        #        print(author)
+    def find_authors_refs(self):
+        """ 
+        This function does not work yet
+        Takes all of the references.json, and sees how many can be found in auther_db
+        """
+        N_total, N_hits = 0, 0
+        for dir in os.listdir(self.file_dir):
+            path = Path(os.path.join(self.file_dir,dir, 'references.json'))
+            ref_json = pd.read_json(path, lines=True)
+            for author in ref_json['author_ln']:
+                N_total+=1
+                if author in self.author_db:
+                    N_hits += 1
+        return N_total, N_hits
 
 
 
@@ -55,3 +72,4 @@ class proccessing_3:
 if __name__ == "__main__":
     processing = proccessing_3(Path('Randomized_Kaggle_Dataset_Subset_Physics.json'), Path('Step_2'))
     authors = processing.subdivide_by_author()
+    score = processing.find_authors_refs()
