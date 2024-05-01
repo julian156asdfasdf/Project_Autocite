cab78d43c8e0af28c9a450243531a97138a58246
Julian <s203929@dtu.dk>
2024-05-01 11:08:24 +0200

    possible solution to problem

--- step3_processing.py
+++ step3_processing.py
@@ -15,18 +15,31 @@ class proccessing_3:
     def __init__(self, database_url, file_dir):
         self.kaggle_db = pd.read_json(database_url, lines=True)
         self.file_dir = file_dir
-        self.author_db = defaultdict(list)
+        self.author_db = defaultdict(set)
     def subdivide_by_author(self):
         # method 1, each key is name of author
-        i = 0
         for index, row in self.kaggle_db.iterrows():
             authors = row['authors']
             arxiv_id = row['arxiv_id']
             for author in authors.split(','):
-                self.author_db[author].append(arxiv_id)
-                i+=1
-            
+                self.author_db[author].add(arxiv_id)
+        
+        #i = 50
+        #succesfull_iterations = []
+        #for author in self.author_db.keys():
+        #    for other_author in self.author_db.keys():
+        #        if str(author) in other_author:
+        #            self.author_db[author] = self.author_db[author].union(self.author_db[other_author])
+        #            succesfull_iterations.append((author, other_author))
+        #            i+=10
         return self.author_db
+    
+        # Problem is that the authors keys are not good example
+        # the following are the different keys:  " 'Eric H'ebrard"," 'Eric Herbert (LIED)"," 'Eric Herbert and Pierre-Philippe Cortet",
+        # Can be seen when doing sorted(self.author_db)
+        # possible solution, make it a set instead of list, and if two keys contain the same, then union between long and short key on the short one
+        # do not collapse the long one as it can run multiple times
+        # possible implementation above, problem is that it runs incredibly slow
 
         #
         #for dir in os.listdir(self.file_dir):
